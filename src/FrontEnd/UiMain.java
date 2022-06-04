8f8bfaf6cd67a13e0c3e1e4015389d6b37b56f6b
Bao <thebaoking@gmail.com>
2022-06-03 23:14:01 -0700

    Test changes

--- src/FrontEnd/UiMain.java
+++ src/FrontEnd/UiMain.java
@@ -3,7 +3,7 @@ package FrontEnd;
 public class UiMain {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println("12345");
 
     }
 }

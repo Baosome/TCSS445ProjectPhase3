056842cbb96b427ff8e23c65bbfc72740a9be40a
Bao <thebaoking@gmail.com>
2022-06-03 23:10:59 -0700

    inital Changes

---
+++ src/FrontEnd/UiMain.java
@@ -0,0 +1,9 @@
+package FrontEnd;
+
+public class UiMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+    }
+}
